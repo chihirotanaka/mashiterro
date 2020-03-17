5c07362788efe355757d432def950e142b175658
vagrant <vagrant@localhost.localdomain>
2020-03-17 07:42:32 +0000

    [finish]5-4

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+
+gem 'kaminari','~> 1.1.1'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'post_images#index'
 
-  resourced :postimages, only: [:new, :create, :index, :show]
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  	resource :favorites, only: [:create, :destroy]
+    resource :post_comments, only: [:create]
+end
+　　resources :users, only: [:show]
 end
